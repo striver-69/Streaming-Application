6ebbc773cb4aa1f5833c1c6d52ce251fe30efc71
Chiranjivee Saksham <chiranjivee960@gmail.com>
2021-03-11 19:53:08 +0530

    added edit delete functionality

--- .history/src/components/streams/StreamList_20210311194759.js
+++ .history/src/components/streams/StreamList_20210311194759.js
@@ -9,16 +9,7 @@ class StreamList extends React.Component{
 
   renderAdmin(stream){
     if(stream.userId === this.props.currentUserId){
-      return(
-        <div className="right floated content">
-          <button className="ui button primary">
-            Edit
-          </button>
-          <button className="ui button negative">
-            Delete
-          </button>
-        </div>
-      )
+      return <div>EDIT/DELETE</div>
     }
   }
 
@@ -26,12 +17,12 @@ class StreamList extends React.Component{
     return this.props.streams.map((stream)=>{
       return(
         <div className="item" key={stream.id}>
-        {this.renderAdmin(stream)}
         <i className="large middle aligned icon camera"/>
         <div className="content">
           {stream.title}
           <div className="description">{stream.description}</div>
         </div>
+        {this.renderAdmin(stream)}
       </div>
       )
     })
